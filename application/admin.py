8ae54d1b6724b19508b3a183677e15c9ba73ec8b
Gabriel Ferreira <contato@gabrielferreira.com>
2015-06-24 22:35:26 -0300

    register apps

--- application/admin.py
+++ application/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Application
-# Register your models here.
+from .models import Application, Platform, Flavour
+
+class PlatformAdmin (admin.ModelAdmin):
+    pass
 
 class ApplicationAdmin (admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     # readonly_fields = ('slug',)
 
+class FlavourAdmin (admin.ModelAdmin):
+    pass
+
+admin.site.register(Platform, PlatformAdmin)
 admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Flavour, FlavourAdmin)
