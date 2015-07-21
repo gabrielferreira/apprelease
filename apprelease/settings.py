92d92fa9aa45d8c56a7983876cce8626d618f5ba
Gabriel Ferreira <gabriel.d3v@gmail.com>
2015-07-21 16:32:24 -0300

    added mysql conifig in settings

--- apprelease/settings.py
+++ apprelease/settings.py
@@ -87,13 +87,24 @@ WSGI_APPLICATION = 'apprelease.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),   # Or an IP Address that your DB is hosted on
+            'PORT': os.environ.get('DB_PORT'),
+        }
+    }
 
 
 # Internationalization
