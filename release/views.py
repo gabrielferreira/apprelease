9a478110dd6de0b196ee68dd9f462960e06503c1
Gabriel Ferreira <gabriel.d3v@gmail.com>
2015-07-21 17:04:12 -0300

    configuring views permissions

--- release/views.py
+++ release/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Application, Platform, Flavour, Release, Environment
 from .serializers import ApplicationSerializer, ReleaseSerializer, FlavourSerializer, EnvironmentSerializer, PlatformSerializer
 
 # Create your views here.
 # ViewSets define the view behavior.
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 class ApplicationViewSet(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
 
 class ReleaseViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Release.objects.all()
     serializer_class = ReleaseSerializer
 
 class FlavourViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Flavour.objects.all()
     serializer_class = FlavourSerializer
 
 class EnvironmentViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Environment.objects.all()
     serializer_class = EnvironmentSerializer
 
 class PlatformViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Platform.objects.all()
     serializer_class = PlatformSerializer
